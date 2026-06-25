d13d028b926bf53d0a2d6fa752ae2d7db2b2c78a
Lloyd Brown <lloydbrownjnr@gmail.com>
2026-06-26 00:43:10 +0300

    files systems lesson

---
+++ files.js
@@ -0,0 +1,27 @@
+const fs = require("fs");
+
+// reading files
+// fs.readFile("./docs/blog.txt", (err, data) => {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log(data.toString());
+//   }
+// });
+// writiing files
+// fs.writeFile("./docs/blog.txt", "hello piaf", () => {
+//   console.log("file was written");
+// });
+
+// fs.writeFile("./docs/blog1.txt", "hello world", () => {
+//   console.log("new file created");
+// });
+// directories
+fs.mkdir("./assets", (err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log("folder created successfully");
+  }
+});
+// deleting files
