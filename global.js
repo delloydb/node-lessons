6e08dbd49698685a81f9993f3f95e24b6dd2f75a
Lloyd Brown <lloydbrownjnr@gmail.com>
2026-06-26 00:00:54 +0300

    global objects update

--- global.js
+++ global.js
@@ -1,1 +1,16 @@
-//global object
+// global object
+console.log(global);
+
+setTimeout(() => {
+  console.log("in timeout type");
+}, 3000);
+
+const int = setInterval(() => {
+  console.log("in the interval typing again");
+}, 1000);
+
+// Optional: Clear interval after some time to stop it
+setTimeout(() => {
+  clearInterval(int);
+  console.log("Interval stopped");
+}, 10000); // Stops after 10 seconds
