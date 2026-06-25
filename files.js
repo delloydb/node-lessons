e1220ef276c4a1ecd387ea4a11b391484dcad8fc
Lloyd Brown <lloydbrownjnr@gmail.com>
2026-06-26 00:59:25 +0300

    file system functions

--- files.js
+++ files.js
@@ -1,21 +1,21 @@
 const fs = require("fs");
 
 // reading files
-// fs.readFile("./docs/blog.txt", (err, data) => {
-//   if (err) {
-//     console.log(err);
-//   } else {
-//     console.log(data.toString());
-//   }
-// });
+fs.readFile("./docs/blog.txt", (err, data) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(data.toString());
+  }
+});
 // writiing files
-// fs.writeFile("./docs/blog.txt", "hello piaf", () => {
-//   console.log("file was written");
-// });
+fs.writeFile("./docs/blog.txt", "hello piaf", () => {
+  console.log("file was written");
+});
 
-// fs.writeFile("./docs/blog1.txt", "hello world", () => {
-//   console.log("new file created");
-// });
+fs.writeFile("./docs/blog1.txt", "hello world", () => {
+  console.log("new file created");
+});
 // directories
 fs.mkdir("./assets", (err) => {
   if (err) {
